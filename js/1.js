a0193f60e7f1b28be13a5f381eb115df6cefeb64
met253 <matvii.savula@gmail.com>
2023-03-04 15:07:35 +0100

    done

---
+++ js/1.js
@@ -0,0 +1,4 @@
+console.log("hello")
+const age = 12;
+const massage = "hello world";
+console.log("my age is" , age)
---
+++ js/2.js
@@ -0,0 +1,11 @@
+console.log("hello")
+const sus = "hello my name is jj";
+const hh = sus.length;
+console.log(hh)
+
+// or 
+
+// console.log("hello")
+// const sus = "hello my name is jj".length;
+// const hh = sus.length;
+// console.log(sus)
